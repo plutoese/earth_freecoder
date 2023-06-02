dcfaef943a8fab61808c6d3ed9870f306bb8659f
plutoese <glen.zhang7@gmail.com>
2023-06-02 19:22:19 +0800

    minishiny version 0.1.3

--- mini/shinydemo/shinydemo01/app.R
+++ mini/shinydemo/shinydemo01/app.R
@@ -1,13 +1,22 @@
 library(shiny)
+library(htmltools)
 library(bslib)
 
-ui <- page_fixed(
-  theme = bs_theme(bootswatch = "minty"),
-  h2("Hello world")
+ui <- page_navbar(
+  title = "My App",
+  bg = "#0062cc",
+  nav(title = "One", p("First page content.")),
+  nav(title = "Two", p("Second page content.")),
+  nav_spacer(),
+  nav_menu(
+    title = "Other links",
+    align = "right",
+    nav("Three", p("Third page content.")),
+  )
 )
 
+
 server <- function(input, output, session) {
-  
 }
 
 shinyApp(ui, server)
